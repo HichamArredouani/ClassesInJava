a4407bb9398b153526db31202c1bba412f511c91
Hicham Arredouani <Hichamarredouani@hotmail.com>
2025-05-05 15:39:17 +0200

    Recipe

---
+++ src/HolaFrescoApp.java
@@ -0,0 +1,10 @@
+public class HolaFrescoApp {
+    public static void main(String[] args) {
+        Recipe myRecipe = new Recipe("Pasta", 30, true);
+        System.out.println("Recipe made");
+
+    }
+
+}
+
+
---
+++ src/Recipe.java
@@ -0,0 +1,14 @@
+public class Recipe {
+
+    private String name;
+    private int cookingTime;
+    private boolean isVegetarian;
+
+
+public Recipe (String name, int cookingTime, boolean isVegetarian) {
+    this.name = name;
+    this.cookingTime = cookingTime;
+    this.isVegetarian = isVegetarian;
+
+}
+}
