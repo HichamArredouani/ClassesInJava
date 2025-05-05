0a57d9eb1e920b278b8fe61341aa39db13e2d4e8
Hicham Arredouani <Hichamarredouani@hotmail.com>
2025-05-05 16:53:37 +0200

    Airplane

---
+++ src/Airplane.java
@@ -0,0 +1,15 @@
+public class Airplane {
+    private String manufacturer;
+    private String model;
+    private int capacity;
+    private boolean inFlight;
+
+    public Airplane (String manufacturer, String model, int capacity, boolean inFlight) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.capacity = capacity;
+        this.inFlight = inFlight;
+
+
+    }
+}
---
+++ src/RihannaAirApp.java
@@ -0,0 +1,7 @@
+public class RihannaAirApp {
+    public static void main(String[] args) {
+        Airplane firstAirplane = new Airplane("Boeing", "737", 100, true);
+
+        System.out.println("Vliegtuigen zijn aangemaakt!");
+    }
+}
